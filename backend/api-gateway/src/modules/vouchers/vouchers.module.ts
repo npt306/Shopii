b715b94467b1c36fa6f19e7e88c9c1d94ea22945
phuchng <phuchng0612@gmail.com>
2025-03-16 21:32:44 +0700

    update api gateway

---
+++ backend/api-gateway/src/modules/vouchers/vouchers.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { VouchersController } from './vouchers.controller';
+import { VouchersService } from './vouchers.service';
+import { HttpModule } from '@nestjs/axios';
+
+@Module({
+  imports: [
+    HttpModule.register({
+      baseURL: 'http://localhost:3002', 
+    }),
+  ],
+  controllers: [VouchersController],
+  providers: [VouchersService],
+  exports: [VouchersService],
+})
+export class VouchersModule {}
