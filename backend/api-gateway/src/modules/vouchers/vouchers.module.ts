db44d0c8213f98654da1b7910451e57e1dd64177
phuchng <phuchng0612@gmail.com>
2025-03-17 20:38:43 +0700

    Update vouchers.module.ts

--- backend/api-gateway/src/modules/vouchers/vouchers.module.ts
+++ backend/api-gateway/src/modules/vouchers/vouchers.module.ts
@@ -6,7 +6,7 @@ import { HttpModule } from '@nestjs/axios';
 @Module({
   imports: [
     HttpModule.register({
-      baseURL: 'http://localhost:3002', 
+      baseURL: 'http://172.17.0.6:3002', 
     }),
   ],
   controllers: [VouchersController],
