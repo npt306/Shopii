3e4b33c849b04f45084b13af090c89443d34d48f
phuchng <phuchng0612@gmail.com>
2025-03-17 20:52:40 +0700

    edit deployment

--- backend/api-gateway/src/modules/vouchers/vouchers.module.ts
+++ backend/api-gateway/src/modules/vouchers/vouchers.module.ts
@@ -6,7 +6,7 @@ import { HttpModule } from '@nestjs/axios';
 @Module({
   imports: [
     HttpModule.register({
-      baseURL: 'http://host.docker.internal:3002', 
+      baseURL: 'http://172.17.0.1:3002', 
     }),
   ],
   controllers: [VouchersController],
