37d715ae0d2763dac096aa8eaf9706a4aaefcff9
npt306 <157341490+npt306@users.noreply.github.com>
2025-03-19 11:46:02 +0700

    Update main.ts

--- backend/product-service/src/main.ts
+++ backend/product-service/src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      callback(null, origin);
+    },
+    credentials: true,
+  });;
 
   await app.listen(process.env.PORT ?? 3001);
 }
