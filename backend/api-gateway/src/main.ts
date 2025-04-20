48e333328fa243cf1c22d6bf5d0afa5c6831435a
Gia Bẻo <baog3083@gmail.com>
2025-04-20 12:53:48 +0700

    fix: fix cors api-gateway

--- backend/api-gateway/src/main.ts
+++ backend/api-gateway/src/main.ts
@@ -15,14 +15,21 @@ async function bootstrap() {
   );
 
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'http://localhost:8000',
+      'http://localhost:8001',
+      'http://34.58.241.34:8000',
+      'http://34.58.241.34:8001',],
+    credentials: true,
+  });
 
   await app.register(multiPart, {
     limits: {
       fileSize: 50 * 1024 * 1024, // 50 MB
     },
   });
-  
+
   const config = new DocumentBuilder()
     .setTitle('API Gateway')
     .setDescription('API Gateway for the Shopii microservices')
